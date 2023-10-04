7b5f5972b9087a02270e2105b94167e3042b7ef8
emmamay <emmamay.puttock@outlook.com>
2023-10-04 14:16:38 +0100

    test commit

--- wp-templates/front-page.js
+++ wp-templates/front-page.js
@@ -24,7 +24,7 @@ export default function Component(props) {
       />
 
       <main className="container">
-        <EntryHeader title="Welcome to the Faust Scaffold Blueprint" />
+        <EntryHeader title="Welcome to the Faust Scaffold Blueprint test" />
 
         <section className={style.cardGrid}>
           <Link
